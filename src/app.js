73cb793d63aa0596acd5557bbd59c3b52019ab56
Prabakaran <praba.1804@gmail.com>
2021-12-29 11:26:44 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// define path for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewspath = path.join(__dirname, "../templates");
+
+// setup handle bar engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewspath);
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "about" });
+});
+app.get("/help", (req, res) => {
+  res.render("help", { message: "how can I help u" });
+});
+
+app.listen(8080, () => {
+  console.log("Server is up on port 8080");
+});
