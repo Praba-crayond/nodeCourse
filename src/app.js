9360667d26d13dcd2abb7ef1c82d71656b1c4f16
Prabakaran <praba.1804@gmail.com>
2021-12-29 11:28:07 +0530

    init commit

--- src/app.js
+++ src/app.js
@@ -20,10 +20,11 @@ app.get("", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
 });
+
 app.get("/help", (req, res) => {
   res.render("help", { message: "how can I help u" });
 });
-
+console.log("adadad");
 app.listen(8080, () => {
   console.log("Server is up on port 8080");
 });
